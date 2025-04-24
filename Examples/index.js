46af67448bc3cd0135faea7e39cdeb665ed6dda3
Renzo Kooi <renzo.kooi@gmail.com>
2025-04-24 15:01:16 +0200

    Add quoting examples

--- Examples/index.js
+++ Examples/index.js
@@ -1,16 +1,18 @@
 import {logFactory, $} from "./DOMhelpers.js";
+import initStyling from "./dynamicStyling.js";
 // ↳ see https://github.com/KooiInc/SBHelpers
 import $S from "../Bundle/index.min.js";
 const fromUnbundled = false;
 const codeOverlay = await createCodeElement();
 const exampleCode = await fetchTemplates();
 const spaceIndicator = `<span style="color:#AAA">&lt;space&gt;</span>`;
+const SB = Symbol.toSB;
 const {log, logTop} = logFactory();
 main();
 
 function main() {
   window.$S = $S; // try out in console
-  initStyling();
+  initStyling($);
   setDelegates();
   addCustomized();
   printExamples();
@@ -28,7 +30,6 @@ function printExamples() {
 
 function printInitializationExamples() {
   const ex1 = `hello world`[Symbol.toSB].wordsUCFirst;
-  const SB = Symbol.toSB;
   const ex2 = `hello world`[SB].append(`!`).firstUp;
   const wrld = `world`;
   const ex3 = `hello ${wrld}`[SB].firstUp.quote.custom(`¡`, `!`);
@@ -201,6 +202,7 @@ function printGetterExamples() {
   historyEx();
   kebabCaseEx();
   notEmptyEx();
+  quoteEx();
   snakeCaseEx();
   trimAllEx();
   trimAllKeepLFEx();
@@ -496,6 +498,53 @@ function notEmptyEx() {
       "getter-notempty"
     )
   );
+}
+
+function quoteEx() {
+  const quotingStyles = Object.keys($S``.quote)
+    .reduce((acc, v) => {
+      return !/re|custom|^_/.test(v) 
+        ? [...acc, $S`<code>$S(" Hello ").quote.${v}</code> => <span class="quoted">${
+          " Hello "[SB].quote[v]}</span>`.toTag("div", "normal b5")]
+        : acc;
+    }, []);
+  quotingStyles.push($S`<code>$S(" Hello ").quote.curlyDoubleEqual.quote.remove</code> => <span class="quoted">${
+    $S(" Hello ").quote.curlyDoubleEqual.quote.remove}</span>`.toTag("div", "normal b5"));
+  quotingStyles.push($S`<code>$S(" Hello ").quote.custom("**")</code> => <span class="quoted">${
+    $S(" Hello ").quote.custom("**")}</span>`.toTag("div", "normal b5"));
+  quotingStyles.push($S`<code>$S(" Hello ").quote.double.quote.remove</code> => <span class="quoted">${
+    $S(" Hello ").quote.double.quote.remove}</span>`.toTag("div", "normal b5"));
+  
+  const lemma = createLemma(
+    $S(" .quote[...]").toTag("h3", "head code"),
+    `Apply a quoting style to the instance string value.`[SB].toTag("div", "normal")
+    .appendDiv(``,
+      `The module includes a number of 
+        <a
+          class="externalLink arrow"
+          target="_top"
+          data-internal="[constructor].quoteInfo" 
+          href="#static-quoteinfo"> predefined quoting styles</a> 
+      one can use to quote the instance string value.`[SB])
+    .appendDiv(``, `The <code>quote</code> getter returns an Object.
+      One of the properties of this object is the <code>[instance].quote.custom</code>
+      method that can be called to apply custom quotes to the instance string value.
+      The other properties are getters (e.g. <code>$S("hi").quote.singleQuote</code>).`)
+    .appendDiv(``, `The <code>quote.remove</code> special getter can be used to remove 
+      <i>predefined</i> quoting styles from the instance string value. It will not work 
+      on custom quotes.`)
+    .appendDiv(`b5`,
+      $S`The <code>[instance].quote.custom</code> method works exactly like
+      <a
+        class="externalLink arrow"
+        target="_top"
+        data-internal="[instance].enclose" 
+        href="#method-enclose"> [instance].enclose</a>`.asNote)
+    .appendDiv(`b5`, $S`<b>Here are examples for all predefined- and special 
+      <code>quote</code> fields</b>`)  
+    .appendDiv(``, quotingStyles.join(``)),
+    "getter-quote");
+  log(lemma);
 }
 
 function snakeCaseEx() {
@@ -1527,264 +1576,4 @@ function runAndReportPerformance() {
             Still, if one does't need to manipulate hundreds of thousands of strings, 
             StringWeaver's performance should not get in the way`.asNote.asDiv)
     .value});
-}
-
-function initStyling() {
-  // style rules are stored in the JQL style element (head)style#JQLStylesheet
-  const arrowRepeat = $S` ⬇ `.repeat(5).enclose(`"`);
-  $.editCssRules(
-    `:root {
-      --grey-default: rgb(112, 92, 92);
-      --code-color: rgb(12, 13, 14);
-    }`,
-    `body {
-      margin: 2rem;
-    }`,
-    `#log2screen {
-      margin: 0px auto;
-      width: 900px;
-    }`,
-    `.green { 
-        color: green; 
-    }`,
-    `.red { 
-        color: red; 
-    }`,
-    `button[data-code-visible="hidden"]:before {
-      content: 'Display code';
-    }`,
-    `button[data-code-visible="visible"]:before {
-      content: 'Hide code';
-    }`,
-    `div.q { display: inline-block;
-      padding: 0 6rem 0px 2rem;
-      font-family: Georgia, verdana;
-      font-style: italic;
-      color: #777; }`,
-    `div.q:before {
-      font-family: Georgia, verdana;
-      content: '\\201C';
-      position: absolute;
-      font-size: 2rem;
-      color: #c0c0c0;
-      margin-left: -2rem;
-      margin-top: 0.5rem;
-     }`,
-    `div.q::after {
-      font-family: Georgia, verdana;
-      content: '\\201D';
-      margin-left: 1rem;
-      font-size: 2rem;
-      margin-top: 0.5rem;
-      position: absolute;
-      display: inline-block;
-      color: #c0c0c0; }`,
-    `pre.codebox {
-      box-shadow: 2px 2px 6px #555;
-      border-radius: 6px;
-      max-height: 500px;
-      overflow: auto;
-    }`,
-    `code.hljs {
-      background-color: #343636;
-    }`,
-    `code {
-      color: revert;
-      background-color: revert;
-      padding: revert;
-      font-family: revert;
-    }`,
-    `code:not(.codeblock, .hljs) {
-        background-color: rgb(227, 230, 232);
-        color: var(--code-color);
-        padding: 0 4px;
-        display: inline-block;
-        border-radius: 4px;
-      }`,
-    `code.codeblock { 
-        background-color: rgb(227, 230, 232);
-        color: rgb(12, 13, 14);
-        border: none;
-        border-radius: 4px;
-        padding: 4px;
-      }`,
-    `h2 {font-size: 1.3rem; line-height: 1.4rem}`,
-    `pre:not(.codebox).syntax {
-      margin-top: 0.2rem;
-    }`,
-    `b.note { color: red; }`,
-    `.normal {
-       font-family: system-ui, sans-serif;
-       color: var(--grey-default);
-       margin: 3px auto;
-     }`,
-    `.b5 { margin-bottom: 0.5rem; }`,
-    `h1.head, h2.head, h3.head {
-      color: var(--grey-default);
-      font-family: system-ui, sans-serif;
-      margin-bottom: 0.4rem;
-    }`,
-    `h3.head { 
-      font-size: 1.1rem;
-     }`,
-    `h3.head.code {
-      font-family: monospace;
-      color: rgb(98 109 147);
-      padding: 0.2em;
-      border-bottom: 1px solid #DDD;
-      font-size: 1.1em;
-      &:before {
-        content: '✓ '; 
-      }
-    }`,
-    `h3.head.between { 
-        margin-top: 0.4rem;
-     }`,
-    `h2.head, h1.head { 
-       border: 1px dotted var(--grey-default);
-       padding: 0.3rem; 
-       text-align: center;
-     }`,
-     `h1.head { line-height: 1.4em; }`,
-    `#log2screen li:not(.head) { 
-      line-height: 1.4em; 
-      list-style: none;
-     }`,
-    `#log2screen li div.normal li {
-        list-style: none;
-        margin-left: -3em;
-      }`,
-    `details {
-      .lemmaContent { margin: auto 1em auto 1.5em; }
-      
-      summary {
-        cursor: pointer;
-        h3.head.code {
-          margin: 0;
-          display: contents;
-        }
-        .toc {
-          position: relative;
-          display: none;
-          margin-left: -1em;
-          float: right;
-          cursor: pointer;
-          &:before {
-            content: "☝";
-            font-size: 1.2em;
-          }
-          &:hover::after {
-            content: 'back to top';
-          }
-        }
-      }
-      
-      &[open] summary .toc {
-        display: inline-block;
-      }
-       
-      pre {
-        margin-top: 0.1rem;
-      }
-    }`,
-    `a.ExternalLink {
-      background-color: transparent;
-    }`,
-    `a.ExternalLink.arrow:hover::after, .toc:hover::after { 
-        fontSize: 0.7rem;
-        position: absolute;
-        zIndex: 2;
-        display: inline-block;
-        padding: 1px 6px;
-        border: 1px solid #777;
-        box-shadow: 1px 1px 5px #777;
-        margin: 1rem 0 0 -1rem;
-        color: #444;
-        background-color: #FFF;
-    }`,
-    `.toc:hover::after {
-      margin: -0.3rem 0px 0px -6rem;
-    }`,
-    `a.ExternalLink[data-backto].arrow:hover::after {
-      content: ' navigates back to 'attr(data-backto);
-    }`,
-    `a.ExternalLink[data-internal].arrow:hover::after {
-      content: ' navigate to 'attr(data-internal);
-    }`,
-    `a.ExternalLink[target="_top"]:not([data-backto], [data-internal]).arrow:hover::after {
-      content: ' navigates back to 'attr(href);
-    }`,
-    `a.ExternalLink[target="_blank"].arrow:hover::after {
-      content: ' Opens in new tab/window';
-    }`,
-    `a.lemma {
-      text-decoration: none;
-      color: #777;
-      margin-left: 1rem;
-      display: block;
-      &:hover {
-        text-decoration: underline;
-      }
-      &:before {
-        content: "✓ ";
-      }
-      &[data-active='1'] {
-        background-color: #eee;
-        color: red;
-      }
-    }`,
-    `table {
-      margin: 1rem 0;
-      font-family: verdana;
-      font-size: 0.9rem;
-      border-collapse: collapse;
-      max-width: 100%;
-     }`,
-    `table caption {
-      border: 1px solid #ccc;
-      padding: 0.5rem;
-      font-size: 14px;
-      white-space: nowrap;
-     }`,
-    `table:not(.hljs-ln) tbody tr:nth-child(even) { background-color: #eee; }`,
-    `table:not(.hljs-ln) td, table th { padding: 2px 4px; font-size: 14px; height: 18px}`,
-    `table:not(.hljs-ln) th { backgroundColor: #999; color: #FFF; }`,
-    `table:not(.hljs-ln) tr td:first-child,
-     table:not(.hljs-ln) tr th:first-child {text-align: right; padding-right: 0.5rem; width:50%}`,
-    `th { 
-      font-weight: bold;
-      text-align: left;
-      border-bottom: 1px solid #999; 
-    }`,
-    `.largeArrowDown:before{
-      content: ${arrowRepeat};
-      color: #555; 
-    }`,
-    `.notifyHeader { 
-      color: #555;
-      &:before {
-        content: '* ';} 
-    }`,
-    `.bottomSpacer {
-      min-height: 100vh;
-    }`,
-    `.spin {
-      display: block;
-      animation: spinner 1.5s linear infinite;
-      color: red;
-      text-align: center;
-    }`,
-    `@keyframes spinner  {
-      from {
-        -moz-transform: rotateY(0deg);
-        -ms-transform: rotateY(0deg);
-        transform: rotateY(0deg);
-      }
-      to {
-        -moz-transform: rotateY(-360deg);
-        -ms-transform: rotateY(-360deg);
-        transform: rotateY(-360deg);
-      }
-    }`
-  );
 }
